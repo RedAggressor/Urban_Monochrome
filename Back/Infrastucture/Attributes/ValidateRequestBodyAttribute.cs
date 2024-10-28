c0aa622f4ba66ebeed3df7c262143ed9ebf958e2
BabychMaksym <cadacred@gmail.com>
2024-10-28 21:10:23 +0200

    add filters

---
+++ Back/Infrastucture/Attributes/ValidateRequestBodyAttribute.cs
@@ -0,0 +1,103 @@
+﻿using Infrastucture.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Collections;
+using System.ComponentModel.DataAnnotations;
+
+namespace Infrastucture.Attributes
+{
+    public class ValidateRequestBodyAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var request = context.HttpContext.Request;
+
+            if (request.Body == null || request.ContentLength == 0)
+            {
+                context.Result = new OkObjectResult(new BaseResponse
+                {
+                    ErrorMessage = "Request body is null or empty!"
+                });
+                return;
+            }
+
+            var body = context.ActionArguments.Values.FirstOrDefault();
+            if (body == null)
+            {
+                context.Result = new OkObjectResult(new BaseResponse
+                {
+                    ErrorMessage = "Request body cannot be null"
+                });
+                return;
+            }
+
+            var (hasNull, errorMessage) = HasNullProperties(body);
+            if (hasNull)
+            {
+                context.Result = new OkObjectResult(new BaseResponse
+                {
+                    ErrorMessage = errorMessage
+                });
+                return;
+            }
+
+            base.OnActionExecuting(context);
+        }
+
+        private (bool HasNull, string ErrorMessage) HasNullProperties(object obj, string parentName = "")
+        {
+            foreach (var property in obj.GetType().GetProperties())
+            {
+                try
+                {
+                    if (property.GetIndexParameters().Length > 0)
+                    {
+                        continue;
+                    }
+
+                    var value = property.GetValue(obj, null);
+                    var isRequired = property.GetCustomAttributes(typeof(RequiredAttribute), false).Length > 0;
+
+                    if (isRequired && value == null)
+                    {
+                        return (true, $"Field {parentName}{property.Name} cannot be null or empty");
+                    }
+
+                    if (value != null)
+                    {
+                        if (typeof(IEnumerable).IsAssignableFrom(property.PropertyType) && property.PropertyType != typeof(string))
+                        {
+                            foreach (var item in (IEnumerable)value)
+                            {
+                                if (item == null)
+                                {
+                                    return (true, $"Field {parentName}{property.Name}[] cannot contain null elements");
+                                }
+
+                                var (hasNull, errorMessage) = HasNullProperties(item, parentName + property.Name + "[]");
+                                if (hasNull)
+                                {
+                                    return (true, errorMessage);
+                                }
+                            }
+                        }
+                        else if (property.PropertyType.IsClass && property.PropertyType != typeof(string))
+                        {
+                            var (hasNull, errorMessage) = HasNullProperties(value, parentName + property.Name + ".");
+                            if (hasNull)
+                            {
+                                return (true, errorMessage);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Помилка у властивості {parentName}{property.Name}: {ex.Message}", ex);
+                }
+            }
+                return (false, string.Empty);
+            
+        }
+    }
+}
---
+++ Back/Infrastucture/Filters/HttpGlobalExceptionFilter.cs
@@ -0,0 +1,40 @@
+﻿using Infrastucture.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using System.Net;
+
+namespace Infrastucture.Filters
+{
+    public class HttpGlobalExceptionFilter : IExceptionFilter
+    {
+        private readonly ILogger<HttpGlobalExceptionFilter> _logger;
+
+        public HttpGlobalExceptionFilter(ILogger<HttpGlobalExceptionFilter> logger)
+        {
+            _logger = logger;
+        }
+        public void OnException(ExceptionContext context)
+        {
+            if (context.Exception is Exception ex)
+            {
+                var problemDetails = new BaseResponse()
+                {
+                    ErrorMessage = ex.Message,
+                };
+
+                context.Result = new OkObjectResult(problemDetails);
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.OK;
+                context.ExceptionHandled = true;
+                _logger.LogError($"{context.Exception} it`s global filter");
+            }
+            else
+            {
+                _logger.LogError(
+                    new EventId(context.Exception.HResult),
+                    $"{context.Exception} it`s global filter",
+                    context.Exception.Message);
+            }
+        }
+    }
+}
