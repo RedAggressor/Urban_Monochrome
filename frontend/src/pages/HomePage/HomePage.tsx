29f3520449f903e4a3f8ae3230813261494b841f
Maksym Mohyla <greenparksquad@gmail.com>
2025-01-13 09:39:15 +0200

    added lookbook

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,6 @@
 import { About } from '../../widgets/About/About';
 import { HomePageBanner } from '../../widgets/HomePageBanner/HomePageBanner';
+import { Lookbook } from '../../widgets/Lookbook/Lookbook';
 import { New } from '../../widgets/New/New';
 import cl from './homePage.module.scss';
 
@@ -9,6 +10,7 @@ export const HomePage = () => {
       <HomePageBanner />
       <About />
       <New />
+      <Lookbook />
     </div>
   );
 };
